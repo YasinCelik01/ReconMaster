3ec485228970766aed58bf276dc8fe74131029c6
YasinCelik01 <yasincelik518@gmail.com>
2024-12-16 17:03:46 -0500

    whois added

---
+++ src/whois_fetcher.py
@@ -0,0 +1,47 @@
+import whois
+import tldextract
+import json
+from urllib.parse import urlparse
+
+def extract_domain(url):
+    
+	try:
+		parsed_url = urlparse(url)
+		if not parsed_url.scheme:
+			url = "http://" + url
+		parsed_url = urlparse(url)
+
+		domain = tldextract.extract(parsed_url.netloc)
+		return f"{domain.domain}.{domain.suffix}"
+	except Exception as e:
+		print(f"Error extracting domain: {e}")
+		return None
+
+def get_whois_info(domain):
+    
+	try:
+		w = whois.whois(domain)
+		return w
+	except Exception as e:
+		print(f"Error fetching WHOIS information: {e}")
+		return None
+        
+#main dosyasından çağrılacak fonksiyon
+
+def fetch_whois_from_url(url):
+
+	domain = extract_domain(url)
+	whois_info = get_whois_info(domain)
+
+	return(json.dumps(whois_info, default=str, indent=4))
+
+	
+
+if __name__ == "__main__":
+
+	url = "python.org"
+	domain = extract_domain(url)
+	print(f"Extracted domain: {domain}")
+	result = fetch_whois_from_url(url)
+	print(result)
+	
