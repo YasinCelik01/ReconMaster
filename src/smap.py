a717969b1389b7b2bf3724dec6efda26ba9c1b16
BALPARS <you@example.com>
2024-12-19 13:40:30 -0500

    smap added

---
+++ src/smap.py
@@ -0,0 +1,38 @@
+import subprocess
+import re
+
+import subprocess
+
+def smap_scan(target: str):
+    SMAP_COMMAND = [
+        'smap',
+        target,
+    ]
+
+    process = subprocess.Popen(
+        SMAP_COMMAND,
+        stdout=subprocess.PIPE,
+        text=True
+    )
+    
+    stdout, stderr = process.communicate()
+    output_list = stdout.splitlines()
+    
+    extracted_data = []
+    for line in output_list:
+        if any(proto in line for proto in ['tcp', 'udp']):  # Check if the line contains protocol info
+
+            parts = line.split()  # Split the line by whitespace
+            if len(parts) >= 3:
+                port_protocol = parts[0]  # First item (e.g., "80/tcp")
+              # state = parts[1] # Open durumu
+                service = parts[2]        # Third item (e.g., "http")
+                version = " ".join(parts[3:]) if len(parts) > 3 else "N/A"  # Kalanlar versiyon bilgisi
+                extracted_data.append(f"{port_protocol} {service} {version.strip()}")
+    
+    return extracted_data
+
+
+if __name__ == "__main__":
+    result = smap_scan('balpars.com')
+    print(result)
