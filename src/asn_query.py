da5b69b51aef75d24a89ad245c7a22f03cd83410
BALPARS <you@example.com>
2024-12-22 06:27:08 -0500

    asn + cidr

--- src/asn_query.py
+++ src/asn_query.py
@@ -5,7 +5,27 @@ def asn_query(asn_number:str):
     response = requests.get(URL)
     return response.json()
 
+def ip_query(ip_address:str):
+    URL = f"https://ip.guide/{ip_address}"
+    response = requests.get(URL)
+    return response.json()
+
+def asn_to_ip(asn_number:str):
+    asn_info = asn_query(asn_number)
+    return asn_info['routes']
+
+def ip_to_asn(ip_address:str):
+    ip_info = ip_query(ip_address)
+    # CIDR yapınca response json yapısında değişiklik oluyor
+    if '/' in ip_address:
+        ASN = ip_info['autonomous_system']['asn']
+    else:
+        ASN = ip_info['network']['autonomous_system']['asn']
+    
+    return "AS"+str(ASN)
+
 if __name__ == "__main__":
-    TARGET = "AS14421"
-    result = asn_query(TARGET)
-    print(result)
+    ip = asn_to_ip("AS14421")
+    asn = ip_to_asn("216.101.17.0/24")
+    print(ip)
+    print(asn)
--- src/cidr_to_ip.py
+++ src/cidr_to_ip.py
@@ -1,6 +1,10 @@
 import ipaddress
 
 def cidr_to_ip(cidr:str):
+    """CIDR formatından üretilebilecek IP adreslerinin listesini döndürür.
+       "192.168.1.0/24" -> ["192.168.1.0", "192.168.1.1", ... ,"192.168.1.255"]
+    """
+    
     return [str(ip) for ip in ipaddress.IPv4Network(cidr)]
 
 if __name__ == "__main__":
